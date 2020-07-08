b1f5cdfb90e01995c75270fa33c5eda85ae34aba
Abd Hannan <[admin@corefreelancers.com]>
2020-07-08 09:46:32 +0700

    draft 1

---
+++ js/index.js
@@ -0,0 +1,44 @@
+import { ZoomMtg } from "@zoomus/websdk";
+
+
+console.log("checkSystemRequirements");
+console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
+
+// it's option if you want to change the WebSDK dependency link resources. setZoomJSLib must be run at first
+// if (!china) ZoomMtg.setZoomJSLib('https://source.zoom.us/1.7.9/lib', '/av'); // CDN version default
+// else ZoomMtg.setZoomJSLib('https://jssdk.zoomus.cn/1.7.9/lib', '/av'); // china cdn option
+// ZoomMtg.setZoomJSLib('http://localhost:9999/node_modules/@zoomus/websdk/dist/lib', '/av'); // Local version default, Angular Project change to use cdn version
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareJssdk();
+
+const API_KEY = "av_XAzOcS7yvwoEvfp47wA";
+
+/**
+ * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
+ * The below generateSignature should be done server side as not to expose your api secret in public
+ * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/Web-Client-SDK/tutorial/generate-signature
+ */
+const API_SECRET = "e1Y9QHS0lZ6fYx0BuaEc7s4fVcinDHwAfLma";
+
+
+document.getElementById("join_meeting").addEventListener("click", (e) => {
+  e.preventDefault();
+  const meetingConfig = testTool.getMeetingConfig(81284625158, "313793");
+  
+  // testTool.setCookie("meeting_number", meetingConfig.mn);
+  // testTool.setCookie("meeting_pwd", meetingConfig.pwd);
+
+  const signature = ZoomMtg.generateSignature({
+    meetingNumber: meetingConfig.mn,
+    apiKey: API_KEY,
+    apiSecret: API_SECRET,
+    role: meetingConfig.role,
+    success: function (res) {
+      meetingConfig.signature = res.result;
+      meetingConfig.apiKey = API_KEY;
+      const joinUrl = "/meeting.html?" + testTool.serialize(meetingConfig);
+      window.open(joinUrl, "_blank");
+    },
+  });
+});
+
---
+++ js/meeting.js
@@ -0,0 +1,142 @@
+import { ZoomMtg } from "@zoomus/websdk";
+const testTool = window.testTool;
+// get meeting args from url
+const tmpArgs = testTool.parseQuery();
+const meetingConfig = {
+  apiKey: "av_XAzOcS7yvwoEvfp47wA",
+  meetingNumber: tmpArgs.mn,
+  userName: (function () {
+    if (tmpArgs.name) {
+      try {
+        return testTool.b64DecodeUnicode(tmpArgs.name);
+      } catch (e) {
+        return tmpArgs.name;
+      }
+    }
+    return (
+      "CDN#" +
+      tmpArgs.version +
+      "#" +
+      testTool.detectOS() +
+      "#" +
+      testTool.getBrowserInfo()
+    );
+  })(),
+  passWord: tmpArgs.pwd,
+  leaveUrl: "/index.html",
+  role: parseInt(tmpArgs.role, 10),
+  userEmail: (function () {
+    try {
+      return testTool.b64DecodeUnicode(tmpArgs.email);
+    } catch (e) {
+      return tmpArgs.email;
+    }
+  })(),
+  lang: tmpArgs.lang,
+  signature: tmpArgs.signature || "",
+  china: tmpArgs.china === "1"
+};
+
+// it's option if you want to change the WebSDK dependency link resources. setZoomJSLib must be run at first
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareJssdk();
+function beginJoin(signature) {
+  ZoomMtg.init({
+    leaveUrl: meetingConfig.leaveUrl,
+    webEndpoint: meetingConfig.webEndpoint,
+    success: function () {
+      console.log(meetingConfig);
+      console.log("signature", signature);
+      $.i18n.reload(meetingConfig.lang);
+      ZoomMtg.join({
+        meetingNumber: meetingConfig.meetingNumber,
+        userName: meetingConfig.userName,
+        signature: signature,
+        apiKey: meetingConfig.apiKey,
+        userEmail: meetingConfig.userEmail,
+        passWord: meetingConfig.passWord,
+        success: function (res) {
+          console.log("join meeting success");
+          console.log("get attendeelist");
+          ZoomMtg.getAttendeeslist({});
+          ZoomMtg.getCurrentUser({
+            success: function (res) {
+              console.log("success getCurrentUser", res.result.currentUser);
+            },
+          });
+        },
+        error: function (res) {
+          console.log(res);
+        },
+      });
+    },
+    error: function (res) {
+      console.log(res);
+    },
+  });
+}
+
+beginJoin(meetingConfig.signature);
+
+document.getElementById("setting_button").addEventListener("click", (e) => {
+  e.preventDefault();
+  alert("work")
+});
+
+$(document).ready(function() {
+  $("#mute_button").on("click", (e) => {
+    e.preventDefault();
+    $('#myModal').modal('show');
+
+    ZoomMtg.getAttendeeslist({
+      success: function(response) {
+        var list = response.result.attendeesList;
+        list.forEach(element => {
+          if(element.isHost == false) 
+          {
+            if(element.muted == true)
+            {
+              var template = 
+              `<li class="list-group-item" style="display: flex; flex-direction: row; justify-content: space-between;">
+                <span style="align-self: center">[` + element.userId + '] ' + element.userName + `</span>` + 
+                `<button class="btn btn-default right mute_action" userId="` + element.userId + `" muted="` + element.muted + `">unmute</button>` +
+              `</li>`;
+              $(".modal-body.participant ul").append(template);
+            }
+            else
+            {
+              var template = 
+              `<li class="list-group-item" style="display: flex; flex-direction: row; justify-content: space-between;">
+                <span style="align-self: center">[` + element.userId + '] ' + element.userName + `</span>` + 
+                `<button class="btn btn-default right mute_action" userId="` + element.userId + `" muted="` + element.muted + `">mute</button>` +
+              `</li>`;
+              $(".modal-body.participant ul").append(template);
+            }
+          }
+        });
+      },
+      error: function(err) {
+        console.log("err" + err);
+      }
+    });
+
+    $('#myModal').on('hidden.bs.modal', function (e) {
+      $(".modal-body.participant").html("");
+    });
+
+    $(".mute_action").on('click', function(e) {
+      var button = $(e.target);
+      var muted = button.attr("muted"); 
+      var userId = button.attr("userId"); 
+      ZoomMtg.mute({
+        userId: parseInt(userId),
+        mute: !muted,
+        success: function(res) {
+          console.log(res);
+        }
+      });
+    
+    });
+
+  });
+});
